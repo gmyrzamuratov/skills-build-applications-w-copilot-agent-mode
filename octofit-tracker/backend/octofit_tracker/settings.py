d2d762e9d6af026865558e555a990e53300fa71c
gmyrzamuratov <gmyrzamuratov@gmail.com>
2025-04-22 18:36:43 +0000

    Add comment to clarify use of Djongo for MongoDB integration in settings.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -86,6 +86,8 @@ DATABASES = {
     }
 }
 
+# Djongo is used as the database engine for MongoDB integration
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
